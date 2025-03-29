5c82bb82240706060d184ee0625fba0c7cc0e174
higgs <matt@secureddesigns.net>
2025-03-29 09:48:13 -0400

    early version of palindrome detector

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in crashmat_palindrome.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "minitest", "~> 5.16"
+
+gem "rubocop", "~> 1.21"
+
+#gem 'minitest-reporters', '1.2.0'
---
+++ test/test_crashmat_palindrome.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestCrashmatPalindrome < Minitest::Test
+  def test_non_palindrome
+    assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  end
+end
+
+
